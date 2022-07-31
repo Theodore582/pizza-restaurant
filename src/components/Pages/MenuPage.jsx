428aa33f06ba7a360cf13bc51eea06e4f21c6320
Annette Theodore <annettetheodore582@gmail.com>
2022-07-31 09:50:35 -1200

    feat: :sparkles: created menu page

---
+++ src/components/Pages/MenuPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Menu2 from '../Menu2/Menu2'
+
+const MenuPage = () => {
+  return (
+    <div>
+    <Header />
+    <Menu2 />
+    </div>
+  )
+}
+
+export default MenuPage
