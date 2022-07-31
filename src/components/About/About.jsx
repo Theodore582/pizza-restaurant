a96dac7982736109a498954934fd28f0fe198d0e
Annette Theodore <annettetheodore582@gmail.com>
2022-07-31 09:43:36 -1200

    feat: :sparkles: created about component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+import aboutUs from '../../images/aboutUs.png'
+
+const About = () => {
+  return (
+    <div className='About'>
+        <div className="about-us">
+    <h2>ABOUT US</h2>
+    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    <button className='about-button'>READ MORE</button>  
+        </div>
+        <div className="about-us-img">
+       <img src={aboutUs} alt="" />
+        </div>
+
+     
+    </div>
+  )
+}
+
+export default About
