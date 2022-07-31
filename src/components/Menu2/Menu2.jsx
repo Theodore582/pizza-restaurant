816050bf3dafd24ea44ec25f5e9d68e669bf3e93
Annette Theodore <annettetheodore582@gmail.com>
2022-07-31 09:49:35 -1200

    feat: :sparkles: created menu2 component

---
+++ src/components/Menu2/Menu2.jsx
@@ -0,0 +1,177 @@
+import React from 'react'
+import './Menu2.css'
+import menupic1 from '../../images/menupic1.png'
+import menupic2 from '../../images/menupic2.png'
+import menupic3 from '../../images/menupic3.png'
+
+const Menu2 = () => {
+    return (
+        <div className='menu2'>
+            <h2>Our Menu</h2>
+            <ul>
+                <li>ALL</li>
+                <li>STARTERS</li>
+                <li>MAIN</li>
+                <li>DRINKS</li>
+                <li>PROMOTION</li>
+            </ul>
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic1} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic1} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic2} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic2} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic3} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic3} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic1} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic1} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic2} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic2} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="menu2-imgs">
+                <div className="menu2-img">
+                    <img src={menupic3} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+
+
+
+                </div>
+                <div className="menu2-img">
+                    <img src={menupic3} alt="" />
+                    <div className='menu2-text'>
+                        <h5 className='menu2-h5'>LOREM IPSUM
+                            <span>$25</span></h5>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac placerat dui.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Menu2
