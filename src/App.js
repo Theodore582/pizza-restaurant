641a633132188d073644dfa8040c68eaaf2cd9c3
Annette Theodore <annettetheodore582@gmail.com>
2022-07-31 09:41:07 -1200

    feat: :sparkles: setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/Pages/HomePage';
+import { Routes,Route } from 'react-router-dom'
+import MenuPage from './components/Pages/MenuPage';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      
+   <Routes>
+    <Route path={'/'} element={<HomePage /> }/>
+    <Route path={'/menu'} element={<MenuPage /> }/>
+   </Routes>
+       </div>
   );
 }
+
+
+
+
 
 export default App;
